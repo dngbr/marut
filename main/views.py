8118c3cd101be1b18fea46b88d0fa56667c0bca4
Bitzu <gabrieldan2399@gmail.com>
2022-08-08 02:45:19 +0300

    prepared app

--- main/views.py
+++ main/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
-    return render(request, "base.html", {})
+    if request.method == "POST":
+        name = request.POST['message-name']
+        email = request.POST['message-email']
+        phone = request.POST['message-phone']
+        message = request.POST['message']
+        send_mail(
+            'Message from ' + name +
+            '(nr.tel.: ' + phone + '| email: ' + email + ')',  # subiect
+            message,  # mesaj
+            email,  # from email
+            ['suport.marut@gmail.com', 'gabrieldan2399@gmail.com',
+                'razvan_felecan@yahoo.com'],  # to email
+        )
+        return render(request, "base.html", {})
+    else:
+        return render(request, "base.html", {})
